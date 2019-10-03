6e28d54fd19eba1a920dd903493b2fdda75edd42
dkotschessa <dkotschessaa@mail.usf.edu>
2019-10-03 05:58:41 -0400

    finished part 5 but everything is messed up :(

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls import url
+
+from . import views
+
 app_name = 'polls'
 urlpatterns = [
-        path('', views.IndexView.as_view(), name='index'),
-        path('<int:pk>/', views.DetailView.as_view(),
-name='detail'),
-        path('<int:pk>/results/',
-views.ResultsView.as_view(), name = 'results'),
-        path('<int:question_id>/vote/', views.vote,
-name='vote'),]
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,6 +6,7 @@ from django.template import loader
 from .models import Choice, Question
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 
@@ -14,8 +15,9 @@ class IndexView(generic.ListView):
     content_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions (not including those
+        set to be published in the future.)"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -24,7 +26,19 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
     
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
